310ab1a6db8662d7ad77ce32ab21a940210552bd
Florian Jug <florian.jug@fht.org>
2022-07-11 10:46:09 +0200

    Introduced cost_factories. Sheida: NOICE!!!!

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 from dataio import projectio
 from viz import napariviz
 from tracking import trackingsolver
+from tracking.cost_factory import *
 
 project_folder = './data/pytr2d_projects/Fluo-N2DL-HeLa'
+
+# cost_factory = cost_factory()
+cost_factory = cost_factory(project_folder + '/tracking/costs.yaml')
+# cost_factory.save_parameters(project_folder + '/tracking/costs.yaml')
+
+print(cost_factory.parameters)
 
 raw = projectio.load_raw(project_folder)
 instances = projectio.load_instances(project_folder)
 timepoints = projectio.load_features(instances)
-tracklets = trackingsolver.ilp(timepoints)
+tracklets = trackingsolver.ilp(timepoints, cost_factory)  # <-- Sheida: NICE! ;)
 napariviz.show_tracking(raw,instances,tracklets)
